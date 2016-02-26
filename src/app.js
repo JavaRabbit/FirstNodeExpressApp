15370976f885dacf95c705bc1dd1a36be4750e73
Bonnie <kbonnie@gmail.com>
2016-02-26 14:26:16 -0800

    updated port

--- src/app.js
+++ src/app.js
@@ -3,11 +3,12 @@
 var express = require('express');
 
 var app = express();
+var port = 8080 || process.env.PORT;
 
 app.get('/', function(req, res){
   res.send("<h1>I love Macs </h1>");
 }); // this is the route to the home
 
-app.listen(8080, function(){
+app.listen(port, function(){
 console.log("it is working on port 8080")
 });
