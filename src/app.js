34f71458fe81752b8f66b9e0af9a65264ae95168
Bonnie <kbonnie@gmail.com>
2016-02-26 14:19:14 -0800

    changed port to 8080

--- src/app.js
+++ src/app.js
@@ -8,6 +8,6 @@ app.get('/', function(req, res){
   res.send("<h1>I love Macs </h1>");
 }); // this is the route to the home
 
-app.listen(3000, function(){
-console.log("it is working on port 3000")
+app.listen(8080, function(){
+console.log("it is working on port 8080")
 });
