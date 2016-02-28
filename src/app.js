06ad9e28ebc4e0274807e2b5f03ba8d2d799b6ba
Bonnie <kbonnie@gmail.com>
2016-02-28 15:52:32 -0800

    added posts json

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+    posts = require('./mockdata/posts.json');
 
 var app = express();
 var port = 8080 || process.env.PORT;
@@ -8,6 +9,10 @@ var port = 8080 || process.env.PORT;
 app.get('/', function(req, res){
   res.send("<h1>I love Macs </h1>");
 }); // this is the route to the home
+
+app.get('/blog', function(req, res){
+  res.send(posts);
+});
 
 app.get('/bear', function(req, res){
   res.send("<h1>I love Bears </h1>");
