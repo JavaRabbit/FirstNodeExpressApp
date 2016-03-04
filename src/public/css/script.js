1dee9089912e14e83c94fd492c3e39790a5161c0
Bonnie <kbonnie@gmail.com>
2016-03-04 08:43:46 -0800

    first bar charts

--- src/public/css/script.js
+++ src/public/css/script.js
@@ -1,1 +1,19 @@
-alert('Heelo from script.js');
+//alert('Heelo from script.js');
+
+var myStyles = [
+  { width: 200, color: "red"},
+  { width: 300, color: "blue"},
+  { width: 250, color: "purple"}
+];
+
+d3.selectAll('.item')
+  .data(myStyles)
+  .style({
+    'color': "white",
+    'background': function(d) {
+    return d.color;
+  },
+    width : function(d) {
+      return d.width + 'px';
+    }
+})
